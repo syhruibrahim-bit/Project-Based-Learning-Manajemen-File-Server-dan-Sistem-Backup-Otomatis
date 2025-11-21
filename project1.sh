aeb7715bee54b89422c65c71504b8f69ccaf8c2a
Syahru Ramadhan Ibrahim <syhruibrahim@gmail.com>
2025-11-21 16:34:12 +0800

    perbaiki nomor 2

--- project1.sh
+++ project1.sh
@@ -17,10 +17,14 @@ mkdir -p {Marketing,Engineering,HR}/{Documents,Archives}
 # 2. Pindahkan dan Salin File
 #Pindahkan file yang salah tempat ke direktori yang benar, buat file backup di folder Archives 
 
-for dept in Marketing Engineering HR; do                        #looping untuk setiap departemen
-    mv "$dept"/"$dept"_* "$dept"/Documents/                     #mv untuk memindahkan file 
-    cp "$dept"/Documents/"$dept"_* "$dept"/Archives/            #cp untuk menyalin file
+for dept in Marketing Engineering HR; do
+    # find untuk mencari file berdasarkan awalan nama departemen dan exec = mengeksekusi mv untuk memindahkan ke departemen yang sesuai
+    find -type f -name "${dept}_*" -not -path "*/Documents/*" -exec mv {} ${dept}/Documents/ \; 2>/dev/null
+
+    # cp untuk menyalin untuk membackup ke folder Archives
+    cp ${dept}/Documents/${dept}_* ${dept}/Archives/
 done
+
 
 # 3. Atur permission
 #Set permission yang tepat agar hanya departemen terkait yang bisa mengakses folder mereka
