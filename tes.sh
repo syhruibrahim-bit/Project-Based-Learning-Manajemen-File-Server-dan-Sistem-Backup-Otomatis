c928d052c8270cff7c30c56b5938a40447cd556b
Syahru Ramadhan Ibrahim <syhruibrahim@gmail.com>
2025-11-21 23:57:26 +0800

    Update tes.sh

--- tes.sh
+++ tes.sh
@@ -1,7 +1,11 @@
+
+
 for dept in Marketing Engineering HR; do
     # Memindahkan file yang namanya diawali nama departemen ke folder Documents
-    find /home/ramdan/Departemen/ -type f -name "${dept}_*" -not -path "*/Documents/*" \ -exec mv {} "/home/ramdan/Departemen/${dept}/Documents/" \; 2>/dev/null
+    find /home/ramdan/Departemen/ -type f -name "${dept}_*" -not -path "*/Documents/*" \
+        -exec mv {} "/home/ramdan/Departemen/${dept}/Documents/" \; 2>/dev/null
 
     # Menyalin file dari Documents ke Archives
-    cp "/home/ramdan/Departemen/${dept}/Documents/${dept}_*" \ "/home/ramdan/Departemen/${dept}/Archives/" 2>/dev/null
+    cp "/home/ramdan/Departemen/${dept}/Documents/${dept}_*" \
+        "/home/ramdan/Departemen/${dept}/Archives/" 2>/dev/null
 done
