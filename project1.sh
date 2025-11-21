cefa78f76e2dabc9ec1e2317cb4252afefc5a30e
Syahru Ramadhan Ibrahim <syhruibrahim@gmail.com>
2025-11-21 23:31:51 +0800

    Update project1.sh

--- project1.sh
+++ project1.sh
@@ -7,7 +7,7 @@
 # 1. Buat struktur direktori
 #Buat folder untuk 3 departemen (Marketing, Engineering, HR) dengan subfolder Documents dan Archives di masing-masing
 
-mkdir -p /Departemen/{Marketing,Engineering,HR}/{Documents,Archives}
+mkdir -p /home/ramdan/Departemen/{Marketing,Engineering,HR}/{Documents,Archives}
 
 #mkdir Membuat direktori baru
 #-p = membuat folder beserta subfolder tanpa error jika sudah ada
@@ -19,10 +19,10 @@ mkdir -p /Departemen/{Marketing,Engineering,HR}/{Documents,Archives}
 
 for dept in Marketing Engineering HR; do
     # find untuk mencari file berdasarkan awalan nama departemen dan exec = mengeksekusi mv untuk memindahkan ke departemen yang sesuai
-    find -type f -name /Departemen/"${dept}_*" -not -path "*/Documents/*" -exec mv {} /Departemen/${dept}/Documents/ \; 2>/dev/null
+    find -type f -name /home/ramdan/Departemen/"${dept}_*" -not -path "*/Documents/*" -exec mv {} /home/ramdan/Departemen/${dept}/Documents/ \; 2>/dev/null
 
     # cp untuk menyalin untuk membackup ke folder Archives
-    cp /Departemen/${dept}/Documents/${dept}_* /departemen/${dept}/Archives/
+    cp /home/ramdan/Departemen/${dept}/Documents/${dept}_* /departemen/${dept}/Archives/
 done
 
 
@@ -34,19 +34,19 @@ sudo groupadd engineering
 sudo groupadd hr
 
 
-sudo chown -R :marketing /home/departemen/Marketing
-sudo chown -R :engineering /home/departemen/Engineering
-sudo chown -R :hr /home/departemen/HR
+sudo chown -R :marketing /home/ramdan/departemen/Marketing
+sudo chown -R :engineering /home/ramdan/departemen/Engineering
+sudo chown -R :hr /home/ramdan/departemen/HR
 
 
-sudo chmod -R 770 /home/departemen/Marketing
-sudo chmod -R 770 /home/departemen/Engineering
-sudo chmod -R 770 /home/departemen/HR
+sudo chmod -R 770 /home/ramdan/departemen/Marketing
+sudo chmod -R 770 /home/ramdan/departemen/Engineering
+sudo chmod -R 770 /home/ramdan/departemen/HR
 
 # 4.Cari dan filter
 #Temukan semua file PDF yang dibuat minggu lalu dan buat daftar lengkapnya
 
-find /home/departemen -type f -name "*.pdf" -mtime -7
+find /home/ramdan/departemen -type f -name "*.pdf" -mtime -7
 
 
 
