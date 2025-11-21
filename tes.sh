853ce8f40aadb92fdf9ebb088a65e30d7c2ddba8
Syahru Ramadhan Ibrahim <syhruibrahim@gmail.com>
2025-11-22 00:22:28 +0800

    Update tes.sh

--- tes.sh
+++ tes.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
-
 for dept in Marketing Engineering HR; do
-    find /home/ramdan/Departemen/ -type f -name "${dept}_*" -not -path "*/Documents/*" \
-        -exec mv {} "/home/ramdan/Departemen/${dept}/Documents/" \; 2>/dev/null
+    find /home/ramdan/Departemen/ -type f -name "${dept}_*" -not -path "*/Documents/*" -exec mv {} "/home/ramdan/Departemen/${dept}/Documents/" \;
 
-    cp "/home/ramdan/Departemen/${dept}/Documents/"* \
-       "/home/ramdan/Departemen/${dept}/Archives/" 2>/dev/null
+    cp "/home/ramdan/Departemen/${dept}/Documents/"* "/home/ramdan/Departemen/${dept}/Archives/" 
 done
