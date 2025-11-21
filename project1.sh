be1005c3748f0628b59cfc81669decd9da56c5ec
Syahru Ramadhan Ibrahim <syhruibrahim@gmail.com>
2025-11-21 15:06:53 +0800

    nomor 2 dgn 3

--- project1.sh
+++ project1.sh
@@ -12,3 +12,17 @@ mkdir -p {Marketing,Engineering,HR}/{Documents,Archives}
 #mkdir Membuat direktori baru
 #-p = membuat folder beserta subfolder tanpa error jika sudah ada
 #{A,B,C} adalah brace expansion, cara cepat membuat banyak folder sekaligus
+
+
+# 2. Pindahkan dan Salin File
+#Pindahkan file yang salah tempat ke direktori yang benar, buat file backup di folder Archives 
+
+for dept in Marketing Engineering HR; do                        #looping untuk setiap departemen
+    mv "$dept"/"$dept"_* "$dept"/Documents/                     #mv untuk memindahkan file 
+    cp "$dept"/Documents/"$dept"_* "$dept"/Archives/            #cp untuk menyalin file
+done
+
+# 3. Atur permission
+#Set permission yang tepat agar hanya departemen terkait yang bisa mengakses folder mereka
+
+
