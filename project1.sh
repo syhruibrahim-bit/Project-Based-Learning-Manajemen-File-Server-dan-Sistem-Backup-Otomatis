0440e850f3408d6867899ce6c90e60e802280849
Syahru Ramadhan Ibrahim <syhruibrahim@gmail.com>
2025-11-21 20:49:26 +0800

    Update project1.sh

--- project1.sh
+++ project1.sh
@@ -19,10 +19,10 @@ mkdir -p /home/Departemen/{Marketing,Engineering,HR}/{Documents,Archives}
 
 for dept in Marketing Engineering HR; do
     # find untuk mencari file berdasarkan awalan nama departemen dan exec = mengeksekusi mv untuk memindahkan ke departemen yang sesuai
-    find -type f -name /home/departemen/"${dept}_*" -not -path "*/Documents/*" -exec mv {} /home/departemen/${dept}/Documents/ \; 2>/dev/null
+    find -type f -name /home/Departemen/"${dept}_*" -not -path "*/Documents/*" -exec mv {} /home/Departemen/${dept}/Documents/ \; 2>/dev/null
 
     # cp untuk menyalin untuk membackup ke folder Archives
-    cp /home/departemen/${dept}/Documents/${dept}_* /home/departemen/${dept}/Archives/
+    cp /home/Departemen/${dept}/Documents/${dept}_* /home/departemen/${dept}/Archives/
 done
 
 
