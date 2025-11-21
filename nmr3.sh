51bab36ef082edc615748c2cafa78cacda516579
Syahru Ramadhan Ibrahim <syhruibrahim@gmail.com>
2025-11-22 00:29:56 +0800

    Update nmr3.sh

--- nmr3.sh
+++ nmr3.sh
@@ -1,16 +1,10 @@
 # 3. Atur permission
 #Set permission yang tepat agar hanya departemen terkait yang bisa mengakses folder mereka
 
-sudo groupadd marketing
-sudo groupadd engineering
-sudo groupadd hr
+for dept in Marketing Engineering HR; do
+    sudo groupadd "$dept"
+    sudo chown -R :"$dept" "/home/ramdan/departemen/$dept"
+    sudo chmod -R 770 "/home/ramdan/departemen/$dept"
+done
 
 
-sudo chown -R :marketing /home/ramdan/departemen/Marketing
-sudo chown -R :engineering /home/ramdan/departemen/Engineering
-sudo chown -R :hr /home/ramdan/departemen/HR
-
-
-sudo chmod -R 770 /home/ramdan/departemen/Marketing
-sudo chmod -R 770 /home/ramdan/departemen/Engineering
-sudo chmod -R 770 /home/ramdan/departemen/HR
