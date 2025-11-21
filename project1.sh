0d51f4060f2d80321ddb0718f7575b9bfbee9a4d
Syahru Ramadhan Ibrahim <syhruibrahim@gmail.com>
2025-11-21 18:51:52 +0800

    Update project1.sh

--- project1.sh
+++ project1.sh
@@ -7,7 +7,7 @@
 # 1. Buat struktur direktori
 #Buat folder untuk 3 departemen (Marketing, Engineering, HR) dengan subfolder Documents dan Archives di masing-masing
 
-mkdir -p {Marketing,Engineering,HR}/{Documents,Archives}
+mkdir -p /home/Departemen/{Marketing,Engineering,HR}/{Documents,Archives}
 
 #mkdir Membuat direktori baru
 #-p = membuat folder beserta subfolder tanpa error jika sudah ada
@@ -19,14 +19,36 @@ mkdir -p {Marketing,Engineering,HR}/{Documents,Archives}
 
 for dept in Marketing Engineering HR; do
     # find untuk mencari file berdasarkan awalan nama departemen dan exec = mengeksekusi mv untuk memindahkan ke departemen yang sesuai
-    find -type f -name "${dept}_*" -not -path "*/Documents/*" -exec mv {} ${dept}/Documents/ \; 2>/dev/null
+    find -type f -name /home/departemen/"${dept}_*" -not -path "*/Documents/*" -exec mv {} /home/departemen/${dept}/Documents/ \; 2>/dev/null
 
     # cp untuk menyalin untuk membackup ke folder Archives
-    cp ${dept}/Documents/${dept}_* ${dept}/Archives/
+    cp /home/departemen/${dept}/Documents/${dept}_* /home/departemen/${dept}/Archives/
 done
 
 
 # 3. Atur permission
 #Set permission yang tepat agar hanya departemen terkait yang bisa mengakses folder mereka
+
+sudo groupadd marketing
+sudo groupadd engineering
+sudo groupadd hr
+
+
+sudo chown -R :marketing /home/departemen/Marketing
+sudo chown -R :engineering /home/departemen/Engineering
+sudo chown -R :hr /home/departemen/HR
+
+
+sudo chmod -R 770 /home/departemen/Marketing
+sudo chmod -R 770 /home/departemen/Engineering
+sudo chmod -R 770 /home/departemen/HR
+
+# 4.Cari dan filter
+#Temukan semua file PDF yang dibuat minggu lalu dan buat daftar lengkapnya
+
+find /home/departemen -type f -name "*.pdf" -mtime -7
+
+
+
 
 
