aba25c6199c7849f5ab274b9453f5cb1689ff153
Syahru Ramadhan Ibrahim <syhruibrahim@gmail.com>
2025-11-22 19:23:39 +0800

    Update project1.sh

--- project1.sh
+++ project1.sh
@@ -17,6 +17,8 @@ mkdir -p /home/ramdan/Departemen/{Marketing,Engineering,HR}/{Documents,Archives}
 # 2. Pindahkan dan Salin File
 #Pindahkan file yang salah tempat ke direktori yang benar, buat file backup di folder Archives 
 
+#Kita misalkan bahwa dalam departemen ada sebuah file sampel yang nantinya kita akan pindah kan
+
 for dept in Marketing Engineering HR; do
     find /home/ramdan/Departemen/ -type f -name "${dept}_*" -not -path "*/Documents/*" -exec mv {} "/home/ramdan/Departemen/${dept}/Documents/" \;
     cp "/home/ramdan/Departemen/${dept}/Documents/"* "/home/ramdan/Departemen/${dept}/Archives/" 
@@ -27,14 +29,14 @@ done
 #Set permission yang tepat agar hanya departemen terkait yang bisa mengakses folder mereka
 
 for dept in Marketing Engineering HR; do
-    sudo groupadd "$dept"
+    sudo groupadd -f "$dept"
     sudo chown -R :"$dept" "/home/ramdan/Departemen/$dept"
     sudo chmod -R 770 "/home/ramdan/Departemen/$dept"
 done
 # 4.Cari dan filter
 #Temukan semua file PDF yang dibuat minggu lalu dan buat daftar lengkapnya
 
-find /home/ramdan/Departemen -type f -name "*.pdf" -mtime -7
+find /home/ramdan/ -type f -name "*.pdf" -mtime -7
 
 
 
