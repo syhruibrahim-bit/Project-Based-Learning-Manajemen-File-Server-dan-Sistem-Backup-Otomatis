0acc48069f2403461dd638fad0c440329a2f9170
Syahru Ramadhan Ibrahim <syhruibrahim@gmail.com>
2025-11-22 20:49:38 +0800

    Delete tes.sh

--- tes.sh
+++
@@ -1,5 +0,0 @@
-for dept in Marketing Engineering HR; do
-    find /home/ramdan/Departemen/ -type f -name "${dept}_*" -not -path "*/Documents/*" -exec mv {} "/home/ramdan/Departemen/${dept}/Documents/" \;
-
-    cp "/home/ramdan/Departemen/${dept}/Documents/"* "/home/ramdan/Departemen/${dept}/Archives/" 
-done
