d3ced894a96993f29550b77d05f53ec88217223b
Syahru Ramadhan Ibrahim <syhruibrahim@gmail.com>
2025-11-22 08:12:05 +0800

    Update project1.sh

--- project1.sh
+++ project1.sh
@@ -18,31 +18,19 @@ mkdir -p /home/ramdan/Departemen/{Marketing,Engineering,HR}/{Documents,Archives}
 #Pindahkan file yang salah tempat ke direktori yang benar, buat file backup di folder Archives 
 
 for dept in Marketing Engineering HR; do
-    # find untuk mencari file berdasarkan awalan nama departemen dan exec = mengeksekusi mv untuk memindahkan ke departemen yang sesuai
-    find -type f -name /home/ramdan/Departemen/"${dept}_*" -not -path "*/Documents/*" -exec mv {} /home/ramdan/Departemen/${dept}/Documents/ \; 2>/dev/null
-
-    # cp untuk menyalin untuk membackup ke folder Archives
-    cp /home/ramdan/Departemen/${dept}/Documents/${dept}_* /departemen/${dept}/Archives/
+    find /home/ramdan/Departemen/ -type f -name "${dept}_*" -not -path "*/Documents/*" -exec mv {} "/home/ramdan/Departemen/${dept}/Documents/" \;
+    cp "/home/ramdan/Departemen/${dept}/Documents/"* "/home/ramdan/Departemen/${dept}/Archives/" 
 done
 
 
 # 3. Atur permission
 #Set permission yang tepat agar hanya departemen terkait yang bisa mengakses folder mereka
 
-sudo groupadd marketing
-sudo groupadd engineering
-sudo groupadd hr
-
-
-sudo chown -R :marketing /home/ramdan/departemen/Marketing
-sudo chown -R :engineering /home/ramdan/departemen/Engineering
-sudo chown -R :hr /home/ramdan/departemen/HR
-
-
-sudo chmod -R 770 /home/ramdan/departemen/Marketing
-sudo chmod -R 770 /home/ramdan/departemen/Engineering
-sudo chmod -R 770 /home/ramdan/departemen/HR
-
+for dept in Marketing Engineering HR; do
+    sudo groupadd "$dept"
+    sudo chown -R :"$dept" "/home/ramdan/departemen/$dept"
+    sudo chmod -R 770 "/home/ramdan/departemen/$dept"
+done
 # 4.Cari dan filter
 #Temukan semua file PDF yang dibuat minggu lalu dan buat daftar lengkapnya
 
